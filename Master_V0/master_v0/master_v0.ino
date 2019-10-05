47beb40aab89a5bb8e993424075f2e3d2528fb73
Khevenin <wojciechbernat95@gmail.com>
2019-10-06 00:48:53 +0200

    Zmiany tablic, zmiennych, funkcji
    Pomiar joysticka zamknieto w funkcji, ktora modyfikuje tablice do pomiarow MeasBuffer. Jednoczesnie zmienai wyniki pomiaro z 10bitow na 8bitowe.
    Zmiany w dataLoad - uproszczenie, poprzez zrezygnowanie z nieużywanej uniwersalnosci funkcji.
    Funkcja przeposuje dane z bufora MeasBuffer do TxBuffer. W przyszlosci moze doklejac cos jeszcze. Wtedy wykorzystac End of Buffer Flag oraz TxBufferCounter, do kontroli sklejania danych.
    Dodanie tablicy z nazwami pinow analogowych - do sterowanai pomiarami.

--- Master_V0/master_v0/master_v0.ino
+++ Master_V0/master_v0/master_v0.ino
@@ -22,16 +22,19 @@
 #define RxLED 6
 
 /*          User Variables              */
-uint8_t timeOut = 16;    //dluzszy time out -> zmienic typ danych na uint16_t
-uint8_t TxBufferCounter = 0; //Licznik bajtow zaladowanych do TxBuffer
-uint8_t TxBuffer[8];  //Bufor nadawczy 
-uint8_t RxBuffer[8];  //Bufor odbiorczy
-const byte addresses[][6] = {"00001", "00002"};   //adresy strumieni przesyłu danych
+uint8_t timeOut = 16;              //Dluzszy time out -> zmienic typ danych na uint16_t
+uint8_t MeasBuffer[3];            //Bufor pomiarowy
+uint8_t TxBuffer[4];               //Bufor nadawczy
+uint8_t RxBuffer[4];               //Bufor odbiorczy
+
+const uint8_t addresses[][6] = {"00001", "00002"};            //adresy strumieni przesyłu danych
+static const uint8_t analogPins[] = {A0, A1, A2, A3, A4};
 
 /*          User Fucntions Prototypes     */
 void pinToggle( uint16_t pin);
 void bufferReset( uint8_t *buf);
-void dataLoad( uint8_t *buf, uint8_t data, uint8_t *bufCounter);  //Zmiana dataMerge na funkcje wsadzajaca dane do TxBuffer
+boolean positionMeasure(uint16_t  *buf, uint8_t analogPinsNum);
+void dataLoad( uint8_t *buf, uint8_t *data);  //Zmiana dataMerge na funkcje wsadzajaca dane do TxBuffer
 
 
 RF24 radio(7, 8); // CE, CSN
@@ -42,9 +45,10 @@ void setup() {
   pinMode(RxLED, OUTPUT);
   delay(1);
 
-  bufferReset(TxBuffer); //zerowanie buforow Tx i Rx
-  bufferReset(RxBuffer); 
-  
+  bufferReset(MeasBuffer);
+  bufferReset(TxBuffer);    //zerowanie buforow Tx i Rx
+  bufferReset(RxBuffer);
+
   /* Radio go on */
   radio.begin();
   radio.openWritingPipe(addresses[1]); // 00002
@@ -56,26 +60,19 @@ void setup() {
 void loop() {
   delay(5);
   /* Pomiary */
-  uint16_t MeasX = analogRead(A1);      //w zakresie 0 - 1024; 0 -0V; 1024 - 5V
-  uint16_t MeasY = analogRead(A0);     // - || -
-  uint16_t MeasSwt =  analogRead(A2);  // - || -
-  uint8_t axisX = map(MeasX, 0, 1023, 0, 255);    // Zmiana wartosci z 10bitowych na 8bitowe - takei beda wykorzystwane jako DC w sterowaniu silnikami
-  uint8_t axisY = map(MeasY, 0, 1023, 0, 255);    
-  uint8_t swt = map(MeasSwt, 0, 1023, 0, 255);
-
-  /* Wysyłanie danych */
-  boolean sendState = 0;      //Zmiany! Redukcja do jednej zmiennej stanu wysłania danych - sendState; Usuniecie sendStateSwt
-  uint8_t timeOutCounter = 0;     // licznk time out
-
+  positionMeasure(MeasBuffer, 3); //na 'sztynwo' ustawiona wartosc portow analogowych
+  /* Sklejanie danych do wysłania */
+  dataLoad(TxBuffer, MeasBuffer);
   /* Radio go on */
   radio.stopListening();                // Zastanowic sie czy nie przeniesc przed petle while()
 
-  /* Sklejanie danych do wysłania */
-
+  /* Wysyłanie danych */
+  boolean sendState = 0;           //Zmiany! Redukcja do jednej zmiennej stanu wysłania danych - sendState; Usuniecie sendStateSwt
+  uint8_t timeOutCounter = 0;     // licznk time out
   while ((sendState == 0 ) || (timeOutCounter < timeOut ))
   {
-//    sendStateAxis = radio.write(&DataAxis, sizeof(DataAxis));      //wysylanie danych polozenia osi oraz zwracanie stanu wyslania
-//    sendStateSwt = radio.write(&DataSwitch, sizeof(DataSwitch));   //wysylanie danych przycisku -||-
+    //    sendStateAxis = radio.write(&DataAxis, sizeof(DataAxis));      //wysylanie danych polozenia osi oraz zwracanie stanu wyslania
+    //    sendStateSwt = radio.write(&DataSwitch, sizeof(DataSwitch));   //wysylanie danych przycisku -||-
     pinToggle(TxLED);                                              // TxLED toggle
   }
 
@@ -93,30 +90,53 @@ void loop() {
 /* User Functions */
 
 // Funkcja ladowania danych do bufora
-//dataLoad wstawia uint8 data do uint buf, w zależnosci od bufCounter
-
-void dataLoad( uint8_t *buf, uint8_t data, uint8_t *bufCounter, uint8_t *endCounterFlag) {
-  if(bufCounter < sizeof(buf)) {
-      buf[bufCounter] = data;
-      bufCounter++; 
+// dataLoad wstawia uint8 data do uint buf, w zależnosci od bufCounter
+void dataLoad( uint8_t *buf, uint8_t *data) {
+  uint8_t bufferSize = sizeof(buf);
+  uint8_t dataSize = sizeof(data);
+  if (dataSize < bufferSize) {
+    for (int i = 0 ; i <= dataSize; i++) {
+        buf[i] = data[i];
+    }
   }
-
 }
 
 //Funckja przelaczanai diody/pinu
 void pinToggle(uint16_t pin) {
-    if (digitalRead(pin) == HIGH) {
-      digitalWrite(pin, LOW);
-    }
-    else {
-      digitalWrite(pin, HIGH);
-    }
+  if (digitalRead(pin) == HIGH) {
+    digitalWrite(pin, LOW);
+  }
+  else {
+    digitalWrite(pin, HIGH);
+  }
 }
 
 //Funkcja resetowanai bufora
 void bufferReset( uint8_t *buf) {
-  uint8_t bufSize = sizeof(buf);            
-  for(uint8_t i = 0; i < bufSize; i++) {   
+  uint8_t bufSize = sizeof(buf);
+  for (uint8_t i = 0; i < bufSize; i++) {
     buf[i] = 0;
+  }
+}
+
+//Funkcja resetowanai bufora
+void bufferReset_16( uint16_t *buf) {
+  uint16_t bufSize = sizeof(buf);
+  for (uint8_t i = 0; i < bufSize; i++) {
+    buf[i] = 0;
+  }
+}
+
+//Funkcja wykonujaca pomiar i wsadzajaca dane do bufora measBuffer, przy jednoczesnym remapowaniu na dane 8bitowe
+boolean positionMeasure(uint8_t  *buf, uint8_t analogPinsNum)  {
+  if (analogPins > 0) {
+    for (int i = 0; i < analogPinsNum; i++) {
+      buf[i] = map(analogPins[i], 0, 1023, 0, 255);
+    }
+    return true;
+  }
+  else {
+    return false;
+
   }
 }
