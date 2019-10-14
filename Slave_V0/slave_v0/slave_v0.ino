d2deab903d76c83657fecdd4d23b000958e3a92e
Khevenin <wojciechbernat95@gmail.com>
2019-10-14 21:11:22 +0200

    Dodanie funkcji i zmiany w buforowaniu
    Dodanie bufferReset oraz funkcji przepisujacej bufferTransfer.
    Dodanie rejestru wykonawczego ExecBuffer[8].
    W pętli odbierania użycie ww.

--- Slave_V0/slave_v0/slave_v0.ino
+++ Slave_V0/slave_v0/slave_v0.ino
@@ -33,12 +33,16 @@ String LD_G = "LED Green";
 String LD_R = "LED Red";
 String LD_Y = "LED Yellow";
 
-uint8_t RxBuffer[8];
+uint8_t RxBuffer[8];            //4 bytes , next change to 8
+uint8_t ExecBuffer[8];        //Executive Buffer 
 const uint8_t RxAddresses[] = {0xAA, 0xAA, 0xAA, 0xAA, 0x01}; //Rx Pipes addresses
 const uint8_t TxAddresses[] = {0xBB, 0xBB, 0xBB, 0xBB, 0x01}; //Tx Pipes addresses
 
-/* User Function Prototypes */ 
-void pinsInitPrint(String pinNum, String Name); 
+/* User Function Prototypes */
+void pinsInitPrint(String pinNum, String Name);
+void bufferReset(uint8_t *buf);
+void bufferTransfer(uint8_t *data, uint8_t dataSize, uint8_t *buf, uint8_t bufSize) ;
+
 
 RF24 radio(7, 8); // CE, CSN
 
@@ -54,7 +58,7 @@ void setup() {
   digitalWrite(LED_GREEN,  LOW);   /* ustawiamy stan niski */
   digitalWrite(LED_RED ,    LOW);
   digitalWrite(LED_YELLOW, LOW);
-  
+
   /* Pins init com. ONLY to DEBUG! */
   pinsInitPrint(String(LED_GREEN), LD_G);
   pinsInitPrint(String(LED_RED), LD_R);
@@ -65,6 +69,8 @@ void setup() {
   radio.openReadingPipe(1, RxAddresses);     // Otwarcie strumienia do odbierania - adres 00002
   radio.setPALevel(RF24_PA_MIN);             // Low power of PowerAmp
   Serial.println("Radio initialization correct\n");
+
+  bufferReset(RxBuffer);
 }
 
 void loop() {
@@ -72,8 +78,9 @@ void loop() {
   radio.startListening();                     /* Zacznij odbierać */
   if (radio.available()) {
     while (radio.available()) {
-    
-      radio.read(RxBuffer, sizeof(RxBuffer));    //zmienne do zmiany
+      radio.read(RxBuffer, sizeof(RxBuffer));                                     //zmienne do zmiany
+      bufferTransfer(RxBuffer, sizeof(RxBuffer), ExecBuffer, sizeof(ExecBuffer));  //przepisanie do rejestru wykonawczego
+      bufferReset(RxBuffer);                                                      //Czyszczenie bufora odbiorczego
     }
     delay(5);
     radio.stopListening();
@@ -81,13 +88,33 @@ void loop() {
 
     /* Odpowiedz zwrotna - klikniecie przycisku */
     // DO ZMIANY!
-//    buttonState = digitalRead(button);
-//    radio.write(&buttonState, sizeof(buttonState));
+    //    buttonState = digitalRead(button);
+    //    radio.write(&buttonState, sizeof(buttonState));
   }
 }
 
 
-
+/* user functions */
 void pinsInitPrint(String pinNum, String Name) {
   Serial.println("Correct initialization of pin " +  pinNum  + ":" + Name);
+}
+
+void bufferReset(uint8_t *buf) {            //Funkcja resetowanai bufora
+  uint8_t bufSize = sizeof(buf);
+  for (uint8_t i = 0; i < bufSize; i++) {
+    buf[i] = 0;
+  }
+}
+
+void bufferTransfer(uint8_t *data, uint8_t dataSize, uint8_t *buf, uint8_t bufSize) {
+  if (dataSize <= bufSize) {
+    for (int i = 0; i < dataSize; i++ ) {
+      buf[i] = data[i];
+    }
+  }
+  else {
+    for (int i = 0; i < bufSize; i++ ) {
+      buf[i] = data[i];
+    }
+  }
 }
